71b30526972a1b581c1bc1d7bed2a165e5fe81e2
Chris Apaza <chrisalanapazaa@gmail.com>
2023-05-31 11:03:44 -0400

    Created spring boot application for subjects

---
+++ src/main/kotlin/com/ucbjudge/ujsubjects/UjSubjectsApplication.kt
@@ -0,0 +1,11 @@
+package com.ucbjudge.ujsubjects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjSubjectsApplication
+
+fun main(args: Array<String>) {
+	runApplication<UjSubjectsApplication>(*args)
+}
